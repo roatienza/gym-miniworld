172ded5dcfb41e91831842e9ec549f4265a0bc74
roatienza <rowel@eee.upd.edu.ph>
2021-05-13 14:03:44 +0800

    discret

--- gym_miniworld/entity.py
+++ gym_miniworld/entity.py
@@ -526,3 +526,102 @@ class Agent(Entity):
 
     def step(self, delta_time):
         pass
+
+
+class RobotAgent(Entity):
+    def __init__(self):
+        super().__init__()
+
+        # Distance between the camera and the floor
+        self.cam_height = 1.5
+
+        # Camera up/down angles in degrees
+        # Positive angles tilt the camera upwards
+        self.cam_pitch = 0
+
+        # Vertical field of view in degrees
+        self.cam_fov_y = 60
+
+        # Bounding cylinder size for the agent
+        self.radius = 0.4
+        self.height = 1.6
+
+        # Object currently being carried by the agent
+        self.carrying = None
+
+        self.vel_x = 0.
+        self.vel_y = 0.
+        self.steer_angle = 0.
+
+    @property
+    def cam_pos(self):
+        """
+        Camera position in 3D space
+        """
+
+        rot_y = gen_rot_matrix(Y_VEC, self.dir)
+        cam_disp = np.array([self.cam_fwd_disp, self.cam_height, 0])
+        cam_disp = np.dot(cam_disp, rot_y)
+
+        return self.pos + cam_disp
+
+    @property
+    def cam_dir(self):
+        """
+        Camera direction (lookat) vector
+
+        Note: this is useful even if just for slight domain
+        randomization of camera angle
+        """
+
+        rot_z = gen_rot_matrix(Z_VEC, self.cam_pitch * math.pi/180)
+        rot_y = gen_rot_matrix(Y_VEC, self.dir)
+
+        dir = np.dot(X_VEC, rot_z)
+        dir = np.dot(dir, rot_y)
+
+        return dir
+
+    def randomize(self, params, rng):
+        params.sample_many(rng, self, [
+            'cam_height',
+            'cam_fwd_disp',
+            'cam_pitch',
+            'cam_fov_y',
+        ])
+        #self.radius = params.sample(rng, 'bot_radius')
+
+    def render(self):
+        """
+        Draw the agent
+        """
+
+        # Note: this is currently only used in the top view
+        # Eventually, we will want a proper 3D model
+
+        p = self.pos + Y_VEC * self.height
+        dv = self.dir_vec * self.radius
+        rv = self.right_vec * self.radius
+
+        p0 = p + dv
+        p1 = p + 0.75 * (rv - dv)
+        p2 = p + 0.75 * (-rv - dv)
+
+        glColor3f(1, 0, 0)
+        glBegin(GL_TRIANGLES)
+        glVertex3f(*p0)
+        glVertex3f(*p2)
+        glVertex3f(*p1)
+        glEnd()
+
+        """
+        glBegin(GL_LINE_STRIP)
+        for i in range(20):
+            a = (2 * math.pi * i) / 20
+            pc = p + dv * math.cos(a) + rv * math.sin(a)
+            glVertex3f(*pc)
+        glEnd()
+        """
+
+    def step(self, delta_time):
+        pass
