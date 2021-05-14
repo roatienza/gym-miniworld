63ab00d7b3e2bf9e7003499c3b42468f730acbce
roatienza <rowel@eee.upd.edu.ph>
2021-05-14 20:42:37 +0800

    learned control

---
+++ learned_control.py
@@ -0,0 +1,113 @@
+
+"""
+This script allows a trained policy to control the simulator.
+Usage:
+
+"""
+
+import sys
+import argparse
+import pyglet
+import math
+from pyglet import clock
+import numpy as np
+import gym
+import gym_miniworld
+import torch
+
+from policy import DDPGActor
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def step(action):
+    print('step {}/{}: {}'.format(env.step_count+1, env.max_episode_steps, env.actions(action).name))
+
+    obs, reward, done, info = env.step(action)
+    if env.is_render_depth:
+        print("obs len", len(obs))
+        for i in range(len(obs)):
+            #print("obs[%d] shape: %s" % (i, obs[i].shape))
+            print(obs[i])
+    else:
+        print("obs shape: ", obs.shape)
+        print('min: %f, max: %f' % (np.amin(obs), np.amax(obs)))
+
+    #if reward > 0:
+    print('reward={:.2f}'.format(reward))
+
+    if done:
+        print('done!')
+        obs = env.reset()
+
+    env.render('pyglet', view=view_mode)
+
+    return obs
+
+
+#if __name__ == "__main__":
+parser = argparse.ArgumentParser()
+parser.add_argument('--env-name', default='MiniWorld-Hallway-v0')
+parser.add_argument('--checkpoint', default='results/DDPG-best_reward.pth')
+parser.add_argument('--no-time-limit', action='store_true', help='ignore time step limits')
+parser.add_argument('--agent-view', action='store_true', help='show the agent view instead of the top view')
+args = parser.parse_args()
+
+env = gym.make(args.env_name)
+
+if args.no_time_limit:
+    env.max_episode_steps = math.inf
+
+view_mode = 'agent' if args.agent_view else 'top'
+
+state = env.reset()
+
+# Create the display window
+env.render('pyglet', view=view_mode) 
+
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.n
+max_action = 1. 
+
+kwargs = {
+    "state_dim": state_dim,
+    "action_dim": action_dim,
+    "max_action": max_action,
+}
+
+actor = DDPGActor(**kwargs).to(device)
+print("Loading checkpoint: ", args.checkpoint)
+actor.load_state_dict(torch.load(args.checkpoint, map_location=torch.device('cpu')))
+actor.eval()
+  
+
+@env.unwrapped.window.event
+def on_key_press(symbol, modifiers):
+    global state
+    print("Symbol: ", symbol)
+    if symbol == 32:
+        step(env.actions.done)
+        pyglet.app.exit()
+    else:
+        with torch.no_grad():
+            state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+            action = actor(state).cpu().data.numpy().flatten()
+            action = np.argmax(action)
+            print("Action:", action)
+            state = step(action)
+
+@env.unwrapped.window.event
+def on_key_release(symbol, modifiers):
+    pass
+
+@env.unwrapped.window.event
+def on_draw():
+    env.render('pyglet', view='top')
+
+@env.unwrapped.window.event
+def on_close():
+    pyglet.app.exit()
+
+
+# Enter main event loop
+pyglet.app.run()
+env.close() 
---
+++ policy.py
@@ -0,0 +1,28 @@
+import copy
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class DDPGActor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(DDPGActor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, action_dim)
+                
+        self.max_action = max_action
+
+        
+    def forward(self, state):
+        a = F.relu(self.l1(state))
+        a = F.relu(self.l2(a))
+        return self.max_action * torch.tanh(self.l3(a))
+
+    #def select_action(self, state):
+    #    state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+    #    action = self.actor(state).cpu().data.numpy().flatten()
